466b40cbc9e054a5e379676d1445a5744ae5582e
Vinihenrique10 <101693879+Vinihenrique10@users.noreply.github.com>
2024-08-04 21:03:33 -0300

    Add files via upload

---
+++ Divertidamente/noticiasrecentesss.online/quizdivertida/quiz/js/popup.js
@@ -0,0 +1,11 @@
+function popup() {
+    console.log('teste')
+    document.getElementById("popupContainer").style.display = "flex";
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("baixar").style.display = "none";
+}
+
+function popupBaixar() {
+    document.getElementById("popup").style.display = "none";
+    document.getElementById("baixar").style.display = "block";
+}
